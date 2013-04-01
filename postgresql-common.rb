de5edb573c33aa245f6083f44276611ae17a8f28
Peter Eisentraut <peter@eisentraut.org>
2013-04-01 08:52:16 -0400

    postgresql-common: Pull directly from GitHub, not from bzr+patch
    It's just more robust that way and has fewer moving pieces.

--- postgresql-common.rb
+++ postgresql-common.rb
@@ -2,16 +2,12 @@ require 'formula'
 
 class PostgresqlCommon < Formula
   homepage 'http://packages.qa.debian.org/p/postgresql-common.html'
-  head 'http://bzr.debian.org/bzr/pkg-postgresql/postgresql-common/trunk/', :using => :bzr
+  head 'https://github.com/petere/postgresql-common.git', :branch => 'homebrew'
 
   conflicts_with 'postgresql',
     :because => 'both install the same binaries.'
 
   depends_on 'gnu-sed' => :build
-
-  def patches
-    "https://github.com/petere/postgresql-common/compare/master...homebrew.patch"
-  end
 
   def install
     system "make", "install", "prefix=#{prefix}"
