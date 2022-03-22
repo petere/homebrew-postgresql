4df298d22c91631af0f20816096c4b09b627adf2
Benjamin Oertel <benjamin.oertel@gmail.com>
2022-03-22 13:53:06 -0700

    and new homebrew service for 12 as well

--- postgresql@12.rb
+++ postgresql@12.rb
@@ -96,33 +96,12 @@ class PostgresqlAT12 < Formula
     EOS
   end
 
-  def plist
-    <<~EOS
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-      <dict>
-        <key>KeepAlive</key>
-        <true/>
-        <key>Label</key>
-        <string>#{plist_name}</string>
-        <key>ProgramArguments</key>
-        <array>
-          <string>#{opt_bin}/postgres</string>
-          <string>-D</string>
-          <string>#{postgresql_datadir}</string>
-        </array>
-        <key>RunAtLoad</key>
-        <true/>
-        <key>WorkingDirectory</key>
-        <string>#{HOMEBREW_PREFIX}</string>
-        <key>StandardOutPath</key>
-        <string>#{postgresql_log_path}</string>
-        <key>StandardErrorPath</key>
-        <string>#{postgresql_log_path}</string>
-      </dict>
-      </plist>
-    EOS
+  service do
+    run [opt_bin/"postgres", "-D", var/"postgresql@12"]
+    keep_alive true
+    log_path var/"log/postgresql@12.log"
+    error_log_path var/"log/postgresql@12.log"
+    working_dir HOMEBREW_PREFIX
   end
 
   test do
